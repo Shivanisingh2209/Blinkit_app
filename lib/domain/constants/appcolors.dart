88927a54c01caab7c0963f78ed9fd12a99bbbd66
Shivanileads <Leadslelo.team@gmail.com>
2025-09-01 13:31:18 +0530

    New app

--- lib/domain/constants/appcolors.dart
+++ lib/domain/constants/appcolors.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color scaffoldBackground = Color(0xFF000000);
+  static const Color scaffoldBackground = Color(0XFFF7CB45);
 }
