c263dc5b93de4930ba30f68a6e564922c27b0218
Shivanileads <Leadslelo.team@gmail.com>
2025-09-02 13:28:02 +0530

    bottom

--- lib/repository/screens/bottomnav/bottomnavscreen.dart
+++ lib/repository/screens/bottomnav/bottomnavscreen.dart
@@ -1,20 +1,32 @@
+import 'package:blinkit_app/repository/screens/cart/cartscreen.dart';
+import 'package:blinkit_app/repository/screens/category/categoryscreen.dart';
+import 'package:blinkit_app/repository/screens/home/HomeScreen.dart';
+import 'package:blinkit_app/repository/screens/print/printscreen.dart';
+import 'package:blinkit_app/repository/widgets/uihelper.dart';
 import 'package:flutter/material.dart';
 
-class BootomNavScreen extends StatefulWidget {
+class BottomNavScreen extends StatefulWidget {
   @override
-  State<BootomNavScreen> createState() => _BootomNavScreenState();
+  State<BottomNavScreen> createState() => _BottomNavScreenState();
 }
 
-class _BootomNavScreenState extends State<BootomNavScreen> {
+class _BottomNavScreenState extends State<BottomNavScreen> {
   int currentIndex = 0;
   List<Widget> pages = [
-
+    HomeScreen(),
+    CategoryScreen(),
+    CartScreen(),
+    PrintScreen(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(icon: UiHelper.CustomImage(img: "home 1.png"), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.category), label: "Category"),
+        BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: "Cart"),
+      ]),
     );
   }
 }
