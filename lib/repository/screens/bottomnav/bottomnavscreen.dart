0f16bc164bd3887243f600b7cf018201ca301acd
Shivanileads <Leadslelo.team@gmail.com>
2025-09-02 13:09:00 +0530

    bottom navigation screen

---
+++ lib/repository/screens/bottomnav/bottomnavscreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class BootomNavScreen extends StatefulWidget {
+  @override
+  State<BootomNavScreen> createState() => _BootomNavScreenState();
+}
+
+class _BootomNavScreenState extends State<BootomNavScreen> {
+  int currentIndex = 0;
+  List<Widget> pages = [
+
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
