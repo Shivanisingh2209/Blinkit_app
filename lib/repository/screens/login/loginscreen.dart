4e18539d253afb15f74a6670a08445047aeb6317
Shivanileads <Leadslelo.team@gmail.com>
2025-09-02 11:03:27 +0530

    login screen

--- lib/repository/screens/login/loginscreen.dart
+++ lib/repository/screens/login/loginscreen.dart
@@ -1,19 +1,105 @@
+import 'package:blinkit_app/repository/widgets/uihelper.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Screen'),
-      ),
-      body: Center(
-        child: Text(
-          'Welcome to the Login Screen!',
-          style: TextStyle(fontSize: 24),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(7.0),
+          child: Center(
+            child: Column(
+              children: [
+                UiHelper.CustomImage(img: "Blinkit Onboarding Screen.png"),
+                SizedBox(height: 5),
+                UiHelper.CustomImage(img: "image 10.png"),
+                SizedBox(height: 4),
+                UiHelper.CustomText(
+                  text: "India's last minute app",
+                  color: Color(0XFF000000),
+                  fontweight: FontWeight.bold,
+                  fontsize: 20,
+                  fontfamily: "bold",
+                ),
+                SizedBox(height: 4),
+                Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Container(
+                    height: 200,
+                    width: 350,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0XFFFFFFFF),
+                    ),
+                    child: Column(
+                      children: [
+                        SizedBox(height: 4),
+                        UiHelper.CustomText(
+                          text: "Shivani",
+                          color: Color(0XFF000000),
+                          fontweight: FontWeight.w500,
+                          fontsize: 14,
+                        ),
+                        SizedBox(height: 4),
+                        UiHelper.CustomText(
+                          text: "7827XXXXXX",
+                          color: Color(0XFF9C9C9C),
+                          fontweight: FontWeight.bold,
+                          fontsize: 14,
+                        ),
+                        SizedBox(height: 4),
+                        SizedBox(
+                          height: 48,
+                          width: 295,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0XFFE23744),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                UiHelper.CustomText(
+                                  text: "Login with",
+                                  color: Color(0XFFFFFFFF),
+                                  fontweight: FontWeight.bold,
+                                  fontsize: 14,
+                                  fontfamily: "bold",
+                                ),
+                                SizedBox(width: 5),
+                                UiHelper.CustomImage(img: "image 9.png"),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 4),
+                        UiHelper.CustomText(
+                          text: "Access your saved addresses from Zomato automatically!",
+                          color: Color(0XFF9C9C9C),
+                          fontweight: FontWeight.normal,
+                          fontsize: 10,
+                          fontfamily: "regular",
+                        ),
+                        SizedBox(height: 4),
+                        UiHelper.CustomText(text: "or login with phone number", color: Color(0XFF269237), fontweight: FontWeight.normal, fontsize: 14, fontfamily: "regular",)
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
-
 }
--- lib/repository/widgets/uihelper.dart
+++ lib/repository/widgets/uihelper.dart
@@ -4,4 +4,22 @@ class UiHelper {
   static CustomImage({required String img}) {
     return Image.asset("assets/images/$img");
   }
+
+  static CustomText({
+    required String text,
+    required Color color,
+    required FontWeight fontweight,
+    required double fontsize,
+    String? fontfamily,
+  }) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: fontsize,
+        fontFamily: fontfamily ?? "regular",
+        color: color,
+        fontWeight: fontweight,
+      ),
+    );
+  }
 }
